40edfeeb2f417fb6ddd9059369612c0aec5e7a25
Babak Sanii <babaksanii@Babaks-iMac.local>
2018-05-25 13:37:06 -0700

    scienced2

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
   # Don't reply to own messages or other bots
-  if data['sender_type'] != 'bot' 
+  if data['sender_type'] != 'bot': 
     inputString = data['text'] #Reads in message from GroupMe payload as a string
     words = inputString.lower().split(" ")
     #make it all lowercase and split into individual words
